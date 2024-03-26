2acb4307e7eafc6096aa72bfe19b259f87b53e34
Marc Gökce <marc.goekce@exxeta.com>
2024-03-26 10:43:09 +0100

    init(project): django unchained

---
+++ companion/presentation/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+
+
+urlpatterns = [
+    # path('', HomepageView.index, name='home'),
+    # path('<int:page>/', HomepageView.index),
+    #
+    # path('post/<int:post_id>/', PostView.post, name='post'),
+    # path('post/<int:post_id>/<int:page>/', PostView.post),
+    # path('post/<int:post_id>/liked', LikeEndpoint.liked),
+    # path('post/<int:post_id>/comment', CommentEndpoint.comment),
+    # path('post/<int:post_id>/comment/<int:comment_id>/rm', CommentEndpoint.comment_delete),
+    #
+    # path('new/', HomepageView.index, name='new'),
+    # path('create/', PostView.create_post, name='create'),
+    #
+    #
+    # path('@<str:user_name>/', ProfileView.user_profile, name='user_profile'),
+    # path('@<str:user_name>/<str:activity_type>/', ProfileView.user_profile, name='user_profile_type'),
+]
